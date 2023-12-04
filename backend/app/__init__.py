b97ee8442f3c35e226ef06d2e15033d23359f5cf
Ahmed Shahriar <ahmedshahriar0247@gmail.com>
2023-12-03 21:59:09 -0500

    done company

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS  # Import CORS from flask_cors
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://invoice_and_tracking:invoice_and_tracking@localhost:5432/invoice_and_tracking'
+
+# Enable CORS for all routes
+CORS(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from .models.modals import *
-
 
 from .views.bill_to import *
 from .views.ship_from import *
@@ -21,4 +24,3 @@ from .views.invoice import *
 from .views.vendor import *
 from .views.purchase_order import *
 from .views.daily_accounts import *
-
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,37 +10,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <head>
-                <meta charset="UTF-8" />
+                <meta charSet="UTF-8" />
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1.0"
                 />
                 <title>Invoice and Tracking</title>
-
-                <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-                <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-                <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
-                <link
-                    rel="stylesheet"
-                    href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css"
-                />
-
-                <script
-                    src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js"
-                    integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
-                    crossorigin="anonymous"></script>
-                <link
-                    rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css"
-                    integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu"
-                    crossorigin="anonymous"
-                />
-
-                <link
-                    rel="stylesheet"
-                    href="/static/styles/global.css"
-                />
             </head>
             <body>
                 <nav>
