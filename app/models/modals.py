74a760bf16526d913023c907e630abe38d51d522
Ahmed Shahriar <ahmedshahriar0247@gmail.com>
2023-12-03 20:22:09 -0500

    asdasd

--- app/models/modals.py
+++ app/models/modals.py
@@ -3,69 +3,71 @@ from app import db, app
 
 class Company(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
-    address1 = db.Column(db.String(200))
-    address2 = db.Column(db.String(200))
-    address3 = db.Column(db.String(200))
-    tel = db.Column(db.String(200))
-    fax = db.Column(db.String(200))
-    gst = db.Column(db.String(200))
-    bank_details = db.Column(db.String(200))
+    name = db.Column(db.String(1200), nullable=True)
+    address1 = db.Column(db.String(1200), nullable=True)
+    address2 = db.Column(db.String(1200), nullable=True)
+    address3 = db.Column(db.String(1200), nullable=True)
+    tel = db.Column(db.String(1200), nullable=True)
+    fax = db.Column(db.String(1200), nullable=True)
+    gst = db.Column(db.String(1200), nullable=True)
+    bank_details = db.Column(db.String(1200), nullable=True)
 
 
 class Bill_to(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
-    address1 = db.Column(db.String(200))
-    address2 = db.Column(db.String(200))
+    name = db.Column(db.String(1200), nullable=True)
+    address1 = db.Column(db.String(1200), nullable=True)
+    address2 = db.Column(db.String(1200), nullable=True)
+    address3 = db.Column(db.String(1200), nullable=True)
 
-
-    
 
 class Ship_from(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
-    address1 = db.Column(db.String(200))
-    address2 = db.Column(db.String(200))
+    name = db.Column(db.String(1200), nullable=True)
+    address1 = db.Column(db.String(1200), nullable=True)
+    address2 = db.Column(db.String(1200), nullable=True)
+    address3 = db.Column(db.String(1200), nullable=True)
 
- 
+
 class Ship_to(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
-    address1 = db.Column(db.String(200))
-    address2 = db.Column(db.String(200))
+    name = db.Column(db.String(1200), nullable=True)
+    address1 = db.Column(db.String(1200), nullable=True)
+    address2 = db.Column(db.String(1200), nullable=True)
+    address3 = db.Column(db.String(1200), nullable=True)
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
-    description = db.Column(db.String(200))
-    price = db.Column(db.Float(precision=5))
-    quantity = db.Column(db.Integer)
+    name = db.Column(db.String(1200), nullable=True)
+    description = db.Column(db.String(1200), nullable=True)
+    price = db.Column(db.Float(precision=5), nullable=True)
+    currency = db.Column(db.String(1200), nullable=True)
+    quantity = db.Column(db.Integer, nullable=True)
 
 
 class Invoice(db.Model):
-    id = db.Column(db.String(20), primary_key=True)
+    id = db.Column(db.String(20), primary_key=True, nullable=True)
 
-    bill_to_id = db.Column(db.Integer, db.ForeignKey('bill_to.id'))
-    ship_from_id = db.Column(db.Integer, db.ForeignKey('ship_from.id'))
-    ship_to_id = db.Column(db.Integer, db.ForeignKey('ship_to.id'))
-    all_items = db.Column(db.String(500))
+    bill_to_id = db.Column(db.Integer, db.ForeignKey('bill_to.id'), nullable=True)
+    ship_from_id = db.Column(db.Integer, db.ForeignKey('ship_from.id'), nullable=True)
+    ship_to_id = db.Column(db.Integer, db.ForeignKey('ship_to.id'), nullable=True)
+    all_items = db.Column(db.String(500), nullable=True)
     
-    date = db.Column(db.DateTime)
-    terms = db.Column(db.String(200))
-    extra_info = db.Column(db.String(200))
-    bl_number = db.Column(db.String(200))
-    bank_details = db.Column(db.String(200))
-    type = db.Column(db.String(200))
+    date = db.Column(db.DateTime, nullable=True)
+    terms = db.Column(db.String(1200), nullable=True)
+    extra_info = db.Column(db.String(1200), nullable=True)
+    bl_number = db.Column(db.String(1200), nullable=True)
+    bank_details = db.Column(db.String(1200), nullable=True)
+    type = db.Column(db.String(1200), nullable=True)
 
     # tracking params
-    container = db.Column(db.String(200))
-    departure = db.Column(db.String(200))
-    location_status = db.Column(db.String(200))
-    custom_tracking = db.Column(db.String(200))
-    BL = db.Column(db.String(200))
-    Deli = db.Column(db.String(200))
-    Manifest = db.Column(db.String(200))
+    container = db.Column(db.String(1200), nullable=True)
+    departure = db.Column(db.String(1200), nullable=True)
+    location_status = db.Column(db.String(1200), nullable=True)
+    custom_tracking = db.Column(db.String(1200), nullable=True)
+    BL = db.Column(db.String(1200), nullable=True)
+    Deli = db.Column(db.String(1200), nullable=True)
+    Manifest = db.Column(db.String(1200), nullable=True)
 
     def __init__(self, *args, **kwargs):
         if not kwargs.get('id'):
@@ -75,33 +77,33 @@ class Invoice(db.Model):
 
 class Vendor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200))
-    address1 = db.Column(db.String(200))
-    address2 = db.Column(db.String(200))
+    name = db.Column(db.String(1200), nullable=True)
+    address1 = db.Column(db.String(1200), nullable=True)
+    address2 = db.Column(db.String(1200), nullable=True)
     
 class Purchase_Order(db.Model):
-    id = db.Column(db.String(20), primary_key=True)
+    id = db.Column(db.String(20), primary_key=True, nullable=True)
     
-    vendor_id = db.Column(db.Integer, db.ForeignKey('vendor.id'))
-    all_items = db.Column(db.String(500))
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendor.id'), nullable=True)
+    all_items = db.Column(db.String(500), nullable=True)
 
-    invoice_id = db.Column(db.String(20), db.ForeignKey('invoice.id'))
+    invoice_id = db.Column(db.String(20), db.ForeignKey('invoice.id'), nullable=True)
 
-    date = db.Column(db.DateTime)
-    terms = db.Column(db.String(200))
-    extra_info = db.Column(db.String(200))
-    bl_number = db.Column(db.String(200))
-    bank_details = db.Column(db.String(200))
-    type = db.Column(db.String(200))
+    date = db.Column(db.DateTime, nullable=True)
+    terms = db.Column(db.String(1200), nullable=True)
+    extra_info = db.Column(db.String(1200), nullable=True)
+    bl_number = db.Column(db.String(1200), nullable=True)
+    bank_details = db.Column(db.String(1200), nullable=True)
+    type = db.Column(db.String(1200), nullable=True)
 
     # tracking params
-    container = db.Column(db.String(200))
-    departure = db.Column(db.String(200))
-    location_status = db.Column(db.String(200))
-    custom_tracking = db.Column(db.String(200))
-    BL = db.Column(db.String(200))
-    Deli = db.Column(db.String(200))
-    Manifest = db.Column(db.String(200))
+    container = db.Column(db.String(1200), nullable=True)
+    departure = db.Column(db.String(1200), nullable=True)
+    location_status = db.Column(db.String(1200), nullable=True)
+    custom_tracking = db.Column(db.String(1200), nullable=True)
+    BL = db.Column(db.String(1200), nullable=True)
+    Deli = db.Column(db.String(1200), nullable=True)
+    Manifest = db.Column(db.String(1200), nullable=True)
     
     def __init__(self, *args, **kwargs):
         if not kwargs.get('id'):
@@ -110,19 +112,16 @@ class Purchase_Order(db.Model):
 
 
 class Daily_Account(db.Model):
-    id = db.Column(db.String(20), primary_key=True)
+    id = db.Column(db.String(20), primary_key=True, nullable=True)
     
-    purchase_order_id = db.Column(db.String(20), db.ForeignKey('purchase__order.id'))
+    purchase_order_id = db.Column(db.String(20), db.ForeignKey('purchase__order.id'), nullable=True)
     
-    all_items = db.Column(db.String(500))
+    all_items = db.Column(db.String(500), nullable=True)
     
     def __init__(self, *args, **kwargs):
         if not kwargs.get('id'):
             kwargs['id'] = f'DA - {random.randint(1000000, 9999999)}'
         super(Daily_Account, self).__init__(*args, **kwargs)
 
-
-
 with app.app_context():
     db.create_all()
-
